ca527604e921103e6b45b277a568d1375b9ff64d
woodwardtw <twwoodward@me.com>
2022-04-01 13:51:55 -0400

    get rid of fake fix

--- js/dlinq-map.js
+++ js/dlinq-map.js
@@ -4,7 +4,6 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
-    tap: false,
     tileSize: 512,
     zoomOffset: -1,
     accessToken: 'pk.eyJ1Ijoid29vZHdhcmR0dyIsImEiOiJjanNhaTVheGgwYTB4NDRwb25qN3lrbjkzIn0.Vi6Vk1OENLLYV1lWVNYSTw'
